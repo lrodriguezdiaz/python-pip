97183361ea59fc0addee1ef1f8adac746b37fd15
Luis Rodriguez <luisrodriguezdiaz@gmail.com>
2023-05-19 20:05:18 -0600

    Varios

---
+++ app/main.py
@@ -0,0 +1,31 @@
+import utils
+import read_csv
+import charts
+import pandas as pd
+
+def run():
+  
+  
+  
+
+  df = pd.read_csv('data.csv')
+  df = df[df['Continent'] == 'Africa']
+
+  countries = df['Country'].values
+  percentages = df['World Population Percentage'].values
+  charts.generate_pie_chart(countries, percentages)
+
+  data = read_csv.read_csv('data.csv')
+  country = input('Type Country => ')
+  print(country)
+
+  result = utils.population_by_country(data, country)
+
+  if len(result) > 0:
+    country = result[0]
+    print(country)
+    labels, values = utils.get_population(country)
+    charts.generate_bar_chart(country['Country'], labels, values)
+
+if __name__ == '__main__':
+  run()
